4c89c1bcb05ec703aae32688953dbdd694536e71
Rob Spearman <rob@DigitalisEducation.com>
2015-01-22 09:34:21 -0800

    Added copyright notices to all code files.

--- FFmpegParameters.hpp
+++ FFmpegParameters.hpp
@@ -1,3 +1,20 @@
+/* Improved ffmpeg plugin for OpenSceneGraph - 
+ * Copyright (C) 1998-2010 Robert Osfield
+ * Modifications copyright (C) 2014-2015 Digitalis Education Solutions, Inc. (http://DigitalisEducation.com)
+ * Modification author: Oktay Radzhabov (oradzhabov at jazzros dot com)
+ *
+ * This library is open source and may be redistributed and/or modified under
+ * the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or
+ * (at your option) any later version.  The full license is in LICENSE file
+ * included with this distribution, and on the openscenegraph.org website.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * OpenSceneGraph Public License for more details.
+*/
+
+
 
 #ifndef HEADER_GUARD_OSGFFMPEG_FFMPEG_PARAMETERS_H
 #define HEADER_GUARD_OSGFFMPEG_FFMPEG_PARAMETERS_H
